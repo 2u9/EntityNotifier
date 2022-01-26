957fceb48a511153871f877e45cda2e573cfa565
NepNep21 <43792621+NepNep21@users.noreply.github.com>
2022-01-26 09:56:25 -0300

    Add dead body notifications

--- src/main/kotlin/me/nepnep/entitynotifier/EntityNotifier.kt
+++ src/main/kotlin/me/nepnep/entitynotifier/EntityNotifier.kt
@@ -3,15 +3,25 @@ package me.nepnep.entitynotifier
 import com.lambda.client.event.events.PacketEvent
 import com.lambda.client.module.Category
 import com.lambda.client.plugin.api.PluginModule
+import com.lambda.client.util.TickTimer
+import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.text.MessageSendHelper
+import com.lambda.client.util.threads.safeListener
 import com.lambda.event.listener.listener
+import net.minecraft.block.Block
 import net.minecraft.client.audio.PositionedSoundRecord
 import net.minecraft.entity.Entity
+import net.minecraft.entity.item.EntityItem
 import net.minecraft.entity.monster.EntityGhast
 import net.minecraft.entity.passive.*
+import net.minecraft.init.Blocks
+import net.minecraft.init.Items
 import net.minecraft.init.SoundEvents
+import net.minecraft.item.ItemBlock
 import net.minecraft.network.play.server.SPacketSpawnMob
 import net.minecraft.util.ResourceLocation
+import net.minecraft.util.math.AxisAlignedBB
+import net.minecraftforge.fml.common.gameevent.TickEvent
 import net.minecraftforge.fml.common.registry.EntityEntry
 import net.minecraftforge.registries.RegistryManager
 import org.apache.logging.log4j.LogManager
@@ -31,6 +41,11 @@ object EntityNotifier : PluginModule(
     private val sheep by setting("Sheep", false)
     private val cows by setting("Cows", false)
     private val villagers by setting("Villagers", false)
+    private val deadBodies by setting("Dead Bodies", false)
+    private val deadBodyAmount by setting("Dead Body Item Amount", 5, 1..20, 1, { deadBodies })
+
+    private val timer = TickTimer(TimeUnit.SECONDS)
+    private val items = mutableListOf<EntityItem>()
 
     init {
         listener<PacketEvent.PostReceive> {
@@ -49,6 +64,52 @@ object EntityNotifier : PluginModule(
                     }
                 }
             }
+        }
+
+        // Can't use EntityJoinWorldEvent because classes from different classloaders cannot be event listeners in forge's bus
+        safeListener<TickEvent.ClientTickEvent> { event ->
+            if (event.phase != TickEvent.Phase.START || !deadBodies || !timer.tick(1)) {
+                return@safeListener
+            }
+
+            val loadedEntities = world.loadedEntityList
+            var notified = false
+            for (entity in loadedEntities) {
+                if (entity is EntityItem && !items.contains(entity)) {
+                    items.add(entity)
+
+                    if (notified) {
+                        break
+                    }
+
+                    val posX = entity.posX
+                    val posY = entity.posY
+                    val posZ = entity.posZ
+                    val aabb = AxisAlignedBB(posX - 2, posY - 2, posZ - 2, posX + 2, posY + 2, posZ + 2)
+                    val amount = world.getEntitiesWithinAABB(EntityItem::class.java, aabb) {
+                        val item = it?.item?.item ?: return@getEntitiesWithinAABB false // Null checks may not be necessary
+                        item == Items.DIAMOND_BOOTS
+                            || item == Items.DIAMOND_CHESTPLATE
+                            || item == Items.DIAMOND_HELMET
+                            || item == Items.DIAMOND_LEGGINGS
+                            || item == Items.DIAMOND_PICKAXE
+                            || item == Items.DIAMOND_AXE
+                            || item == Items.DIAMOND_SHOVEL
+                            || item == Items.DIAMOND_HOE
+                            || item == Items.DIAMOND_SWORD
+                            || item == Items.ELYTRA
+                            || item == Items.TOTEM_OF_UNDYING
+                            || (item is ItemBlock && item.block.isShulkerBox())
+                    }.size
+                    if (amount >= deadBodyAmount) {
+                        notified = true
+                        mc.soundHandler.playSound(PositionedSoundRecord.getRecord(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f))
+                        MessageSendHelper.sendChatMessage("Dead body with $amount valuable items found at $posX $posY $posZ")
+                    }
+                }
+            }
+
+            items.removeIf { !loadedEntities.contains(it) }
         }
     }
 
@@ -78,5 +139,24 @@ object EntityNotifier : PluginModule(
             }
         }
         return PositionedSoundRecord.getRecord(event, 1.0f, 1.0f)
+    }
+
+    private fun Block.isShulkerBox(): Boolean {
+        return this == Blocks.WHITE_SHULKER_BOX
+            || this == Blocks.ORANGE_SHULKER_BOX
+            || this == Blocks.MAGENTA_SHULKER_BOX
+            || this == Blocks.LIGHT_BLUE_SHULKER_BOX
+            || this == Blocks.YELLOW_SHULKER_BOX
+            || this == Blocks.LIME_SHULKER_BOX
+            || this == Blocks.PINK_SHULKER_BOX
+            || this == Blocks.GRAY_SHULKER_BOX
+            || this == Blocks.SILVER_SHULKER_BOX
+            || this == Blocks.CYAN_SHULKER_BOX
+            || this == Blocks.PURPLE_SHULKER_BOX
+            || this == Blocks.BLUE_SHULKER_BOX
+            || this == Blocks.BROWN_SHULKER_BOX
+            || this == Blocks.GREEN_SHULKER_BOX
+            || this == Blocks.RED_SHULKER_BOX
+            || this == Blocks.BLACK_SHULKER_BOX
     }
 }
