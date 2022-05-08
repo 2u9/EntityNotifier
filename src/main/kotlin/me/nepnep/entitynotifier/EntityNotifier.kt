3d0138a5fce04a79f0c7584bda70f9f966d9c6f1
NepNep21 <43792621+NepNep21@users.noreply.github.com>
2022-05-08 16:12:11 -0300

    Add waypoint support

--- src/main/kotlin/me/nepnep/entitynotifier/EntityNotifier.kt
+++ src/main/kotlin/me/nepnep/entitynotifier/EntityNotifier.kt
@@ -8,6 +8,7 @@ import com.lambda.client.util.TimeUnit
 import com.lambda.client.util.text.MessageSendHelper
 import com.lambda.client.util.threads.safeListener
 import com.lambda.client.event.listener.listener
+import com.lambda.client.manager.managers.WaypointManager
 import net.minecraft.block.Block
 import net.minecraft.client.audio.PositionedSoundRecord
 import net.minecraft.entity.Entity
@@ -20,7 +21,9 @@ import net.minecraft.init.SoundEvents
 import net.minecraft.item.ItemBlock
 import net.minecraft.network.play.server.SPacketSpawnMob
 import net.minecraft.util.ResourceLocation
+import net.minecraft.util.SoundEvent
 import net.minecraft.util.math.AxisAlignedBB
+import net.minecraft.util.math.BlockPos
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import net.minecraftforge.fml.common.registry.EntityEntry
 import net.minecraftforge.registries.RegistryManager
@@ -42,6 +45,7 @@ object EntityNotifier : PluginModule(
     private val cows by setting("Cows", false)
     private val villagers by setting("Villagers", false)
     private val deadBodies by setting("Dead Bodies", false)
+    private val addWaypoint by setting("Add Waypoint", false)
     private val deadBodyAmount by setting("Dead Body Item Amount", 5, 1..20, 1, { deadBodies })
 
     private val timer = TickTimer(TimeUnit.SECONDS)
@@ -56,12 +60,10 @@ object EntityNotifier : PluginModule(
                     .getValue(packet.entityType)
                 val clazz = entityEntry.entityClass
                 if (clazz.shouldNotify()) {
-                    if (playSound) {
-                        mc.soundHandler.playSound(clazz.getSound())
-                    }
-                    if (sendMessage) {
-                        MessageSendHelper.sendChatMessage("${entityEntry.name} loaded at ${"%.2f %.2f %.2f".format(packet.x, packet.y, packet.z)}")
-                    }
+                    val x = packet.x
+                    val y = packet.y
+                    val z = packet.z
+                    notify(clazz.getSound(), "${entityEntry.name} loaded at ${"%.2f %.2f %.2f".format(x, y ,z)}", x, y, z, entityEntry.name)
                 }
             }
         }
@@ -103,8 +105,14 @@ object EntityNotifier : PluginModule(
                     }.size
                     if (amount >= deadBodyAmount) {
                         notified = true
-                        mc.soundHandler.playSound(PositionedSoundRecord.getRecord(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f))
-                        MessageSendHelper.sendChatMessage("Dead body with $amount valuable items found at ${"%.2f %.2f %.2f".format(posX, posY, posZ)}")
+                        notify(
+                            SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP,
+                            "Dead body with $amount valuable items found at ${"%.2f %.2f %.2f".format(posX, posY, posZ)}",
+                            posX,
+                            posY,
+                            posZ,
+                            entity.item.displayName
+                        )
                     }
                 }
             }
@@ -123,22 +131,19 @@ object EntityNotifier : PluginModule(
         else -> false
     }
 
-    private fun Class<out Entity>.getSound(): PositionedSoundRecord {
-        val event = when (this) {
-            EntityDonkey::class.java -> SoundEvents.ENTITY_DONKEY_DEATH
-            EntityHorse::class.java -> SoundEvents.ENTITY_HORSE_DEATH
-            EntityGhast::class.java -> SoundEvents.ENTITY_GHAST_DEATH
-            EntitySheep::class.java -> SoundEvents.ENTITY_SHEEP_DEATH
-            EntityCow::class.java -> SoundEvents.ENTITY_SHEEP_DEATH
-            EntityVillager::class.java -> SoundEvents.ENTITY_VILLAGER_DEATH
-            else -> {
-                val error = "Unknown sound for entity $typeName, please report this bug"
-                MessageSendHelper.sendErrorMessage(error)
-                LogManager.getLogger().error(error)
-                SoundEvents.ENTITY_LLAMA_SPIT
-            }
+    private fun Class<out Entity>.getSound() = when (this) {
+        EntityDonkey::class.java -> SoundEvents.ENTITY_DONKEY_DEATH
+        EntityHorse::class.java -> SoundEvents.ENTITY_HORSE_DEATH
+        EntityGhast::class.java -> SoundEvents.ENTITY_GHAST_DEATH
+        EntitySheep::class.java -> SoundEvents.ENTITY_SHEEP_DEATH
+        EntityCow::class.java -> SoundEvents.ENTITY_SHEEP_DEATH
+        EntityVillager::class.java -> SoundEvents.ENTITY_VILLAGER_DEATH
+        else -> {
+            val error = "Unknown sound for entity $typeName, please report this bug"
+            MessageSendHelper.sendErrorMessage(error)
+            LogManager.getLogger().error(error)
+            SoundEvents.ENTITY_LLAMA_SPIT
         }
-        return PositionedSoundRecord.getRecord(event, 1.0f, 1.0f)
     }
 
     private fun Block.isShulkerBox(): Boolean {
@@ -158,5 +163,17 @@ object EntityNotifier : PluginModule(
             || this == Blocks.GREEN_SHULKER_BOX
             || this == Blocks.RED_SHULKER_BOX
             || this == Blocks.BLACK_SHULKER_BOX
+    }
+
+    private fun notify(sound: SoundEvent, message: String, x: Double, y: Double, z: Double, name: String) {
+        if (playSound) {
+            mc.soundHandler.playSound(PositionedSoundRecord.getRecord(sound, 1f, 1f))
+        }
+        if (sendMessage) {
+            MessageSendHelper.sendChatMessage(message)
+        }
+        if (addWaypoint) {
+            WaypointManager.add(BlockPos(x, y, z), "EntityNotifier $name")
+        }
     }
 }
